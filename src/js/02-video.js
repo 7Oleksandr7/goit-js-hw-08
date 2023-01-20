23a6d868d46afcc17f572bb72111690137a72110
7Oleksandr7 <040818a@gmail.com>
2023-01-20 19:51:50 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,11 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const onPlay = ({ seconds }) => {
-  localStorage.setItem('videoplayer-current-time', seconds);
-};
-
-player.on('timeupdate', throttle(onPlay, 1000));
-
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.on(
+  'timeupdate',
+  throttle(function (currentTime) {
+    localStorage.setItem('videoplayer-current-time', currentTime.seconds);
+  }, 1000)
+);
+const currentTime = localStorage.getItem('videoplayer-current-time') || 0;
+player.setCurrentTime(currentTime);
