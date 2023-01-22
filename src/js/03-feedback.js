e044e9d9f4de0668439a5be55a000e173fac0b4b
7Oleksandr7 <040818a@gmail.com>
2023-01-23 00:14:57 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,34 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input');
+const messageInput = document.querySelector('textarea');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
+}
+
+form.addEventListener('input', throttle(addLocalStorage, 500));
+
+function addLocalStorage(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function fillInput() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    emailInput.value = JSON.parse(savedData).email;
+    messageInput.value = JSON.parse(savedData).message;
+  }
+}
+fillInput();
